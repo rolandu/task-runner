d1b5186ebc26163e2fc00f047702e289c72781b0
rolandu <4238671+rolandu@users.noreply.github.com>
2023-08-14 14:07:40 +0200

    Improvements

--- task-runner.lib.sh
+++ task-runner.lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
  
 # This function runs the task, logs its output to a file and returns only ok/error
 # Params:
@@ -19,23 +18,38 @@ function task-runner {
 	DESCRIPTION="$2"
 	LOG_FILE_NAME="$3"
 
-	echo "Command: $TO_BE_RUN" >> "$LOG_FILE_NAME"
+	# Header
 	echo "Task: $DESCRIPTION" >> "$LOG_FILE_NAME"
-	echo "Task started: $(date)" >> "$LOG_FILE_NAME"
+	echo "Command: $TO_BE_RUN" >> "$LOG_FILE_NAME"
+	TS_START=`date +%s`
+	echo "Task started: $(date --iso-8601=seconds)" >> "$LOG_FILE_NAME"
 
-	if eval $TO_BE_RUN >> "$LOG_FILE_NAME" 2>&1
-	then
-		echo "[OK] $DESCRIPTION"
-		RETURNCODE=0
-	else
-		echo "[ERROR] $DESCRIPTION"
-		RETURNCODE=1
-	fi
+	# Execution
+	RESULT=$(eval $TO_BE_RUN >> "$LOG_FILE_NAME" 2>&1)
 
-	echo "Task finished: $(date)" >> "$LOG_FILE_NAME" 
-	echo "------------------------------------" >> "$LOG_FILE_NAME"
+	# Evaluation
+	TS_FINISH=`date +%s`
+	runtime=$((TS_FINISH - TS_START))
+        runtime_hrs=$(( $runtime / 3600 ))
+        runtime_mins=$((( $runtime % 3600 ) / 60 ))
+        runtime_secs=$(( $runtime % 60 ))
+        printf -v runtime_mins "%02d" $runtime_mins
+        printf -v runtime_secs "%02d" $runtime_secs
+	runtime_print=$runtime_hrs:$runtime_mins:$runtime_secs
+
+	# Footer
+	echo "Task finished: $(date --iso-8601=seconds), runtime: $runtime_print" >> "$LOG_FILE_NAME" 
+	echo "------------------------------------------------------------------" >> "$LOG_FILE_NAME"
+
+	# Return
+        if $RESULT
+        then
+                echo "[OK] $DESCRIPTION; runtime: $runtime_print; details: $LOG_FILE_NAME"
+                RETURNCODE=0
+        else
+                echo "[ERROR] $DESCRIPTION; runtime: $runtime_print; details: $LOG_FILE_NAME"
+                RETURNCODE=1
+        fi
 
 }
-
-
 
